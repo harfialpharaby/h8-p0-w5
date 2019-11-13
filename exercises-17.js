aa5806da553f0c2607fa9be3e0e682ea65aff6ac
Harfi <harfialpharaby@gmail.com>
2019-11-13 15:08:29 +0700

    ubah logic exercises-17

--- exercises-17.js
+++ exercises-17.js
@@ -1,14 +1,24 @@
 function kaliTerusRekursif(angka) {
     // you can only write your code here!
-    while (angka >= 10) {
-        angka = (angka % 10) * kaliTerusRekursif(Math.floor(angka / 10));
+    if (angka < 10) {
+        return angka;
     }
 
-    return angka;
+    let str = String(angka);
+    let temp = 1;
+
+    for (let i = 0; i < str.length; i++) {
+        temp *= str[i];
+    }
+
+    if (temp >= 10) {
+        return kaliTerusRekursif(temp);
+    }
+    return temp;
 }
 
 // TEST CASES
-console.log(kaliTerusRekursif(66)); // 8
+console.log(kaliTerusRekursif(669)); // 8
 console.log(kaliTerusRekursif(3)); // 3
 console.log(kaliTerusRekursif(24)); // 8
 console.log(kaliTerusRekursif(654)); // 0
