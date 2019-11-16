44b5458a38bad34b7b25553d3c85a9f1dfe6e972
Harfi <harfialpharaby@gmail.com>
2019-11-16 20:20:13 +0700

    masukkan logic manual

--- exercises-8.js
+++ exercises-8.js
@@ -1,6 +1,29 @@
 function cariPelaku(str) {
     // you can only write your code here!
     return str.match(/abc/g).length;
+
+    // logic manual
+    // let res = 0;
+    // let count = 0;
+
+    // for (let i = 0; i < str.length; i++) {
+    //     if (str[i] === 'a' && count === 0) {
+    //         count++;
+    //     } else if (str[i] === 'b' && count === 1) {
+    //         count++;
+    //     } else if (str[i] === 'c' && count === 2) {
+    //         count++;
+    //     } else {
+    //         count = 0;
+    //     }
+
+    //     if (count === 3) {
+    //         res++;
+    //         count = 0;
+    //     }
+    // }
+
+    return res;
 }
 
 // TEST CASES
